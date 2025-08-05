103eea3fd577033b2accaa9f9673fad1d598bb13
Tejas Balamukesh <tejasbalamukesh@gmail.com>
2025-08-05 21:14:41 +0530

    Changed the Data ingestion from data files to statsbombpy module and edit parser functions to suit that

--- parser.py
+++ parser.py
@@ -31,105 +31,104 @@ def parse_events(json_data):
 
 def extract_tactics(df):
     '''Function to extract tactics-related events from the DataFrame.'''
-    mask = df["type.name"].isin(["Starting XI"])
-    starting11_df = df[mask]
-    starting11_df["lineup"] = starting11_df["tactics.lineup"].copy()
-    lineup_df = starting11_df.explode("lineup").reset_index(drop=True)
-    valid_lineup = [x for x in lineup_df["lineup"] if isinstance(x, dict)]
-    player_metadata = pd.json_normalize(valid_lineup, sep=".", meta=["possession_team.name"])
-    subs_df = df[df["type.name"] == "Substitution"]
-    tactical_events = df[df["type.name"].isin(["Starting XI", "Tactical Shift", "Substitution"])].copy()
-    team_states = [] 
+    tactical_events = df[df['type.name'].isin(["Starting XI", "Substitution", "Tactical Shift"])].copy()
+    team_states = []
     current_lineups = {}
-
     for _, event in tactical_events.iterrows():
-        if event["type.name"] == "Starting XI":
-            team_id = event["team.id"]
-            current_lineups[team_id] = {
-            "formation": event["tactics.formation"], 
-            "players": [{
-                "player_id": p["player"]["id"],
-                "player_name": p["player"]["name"],
-                "position_id": p["position"]["id"],
-                "position_name": p["position"]["name"],
-                "jersey_number": p["jersey_number"]
-            }
-            for p in event["tactics.lineup"]
-            ]
-            }
-            team_states.append({
-                'team_id': team_id, 
-                'team_name':event["team.name"],
-                'minute': event['minute'],
-                "second": event['second'], 
-                'event_type': "Starting XI", 
-                "formation": event["tactics.formation"], 
-                "lineup": current_lineups[team_id]["players"],
-                'substituted_in': None, 
-                'substituted_out': None
-
-            })
-
-        elif event["type.name"] == "Substitution":
-            team_id = event["team.id"]
-            sub_player_id = event['player.id']
-            incoming_player_id = event['substitution.replacement.id']
-            incoming_player_name = event['substitution.replacement.name']
-            lineup = current_lineups[team_id]["players"]
-            outgoing_player = None
-            for i, p in enumerate(lineup):
-                if p["player_id"] == sub_player_id:
-                    outgoing_player = p
-                    lineup[i] = {
-                        'player_id': incoming_player_id, 
-                        'player_name': incoming_player_name, 
-                        'position_id': outgoing_player["position_id"],
-                        'position_name': outgoing_player["position_name"],
-                        'jersey_number': None
-                    }
-                    break
-
-            team_states.append({
-                'team_id': team_id, 
-                'team_name': event["team.name"],
-                'minute': event['minute'], 
-                'second': event['second'],
-                'event_type': "Substitution",
-                "formation": current_lineups[team_id]["formation"],
-                "lineup": current_lineups[team_id]["players"],
-                "substituted_in": incoming_player_id,
-                "substituted_out": sub_player_id
-            })
-    
-        elif event["type.name"] == "Tactical Shift":
-            team_id = event["team.id"]
-            current_lineups[team_id] = {
-                'formation': event["tactics.formation"],
+            if event["type.name"] == "Starting XI":
+                team_id = event["team"]
+                current_lineups[team_id] = {
+                "formation": event['tactics'].get('formation'), 
                 "players": [{
                     "player_id": p["player"]["id"],
                     "player_name": p["player"]["name"],
                     "position_id": p["position"]["id"],
                     "position_name": p["position"]["name"],
                     "jersey_number": p["jersey_number"]
-                } for p in event["tactics.lineup"]
+                }
+                for p in event["tactics"].get('lineup')
                 ]
-            }   
+                }
+                team_states.append({
+                    'team_id': team_id, 
+                    'team_name':event["team_name"],
+                    'minute': event['minute'],
+                    "second": event['second'], 
+                    'event_type': "Starting XI", 
+                    "formation": event["tactics"].get('formation'), 
+                    "lineup": current_lineups[team_id]["players"],
+                    'substituted_in': None, 
+                    'substituted_out': None
 
-            team_states.append({
-            'team_id': team_id,
-            'team_name': event['team.name'],
-            'minute': event['minute'],
-            'second': event['second'],
-            'event_type': 'Tactical Shift',
-            'formation': event['tactics.formation'],
-            'lineup': current_lineups[team_id]['players'].copy(),
-            'substituted_in': None,  
-            'substituted_out': None
-        })
-            
+                })
+        
+        
+            elif event["type.name"] == "Substitution":
+                team_id = event["team"]
+                sub_player_id = event['player']
+                incoming_player_id = event['substitution_replacement_id']
+                incoming_player_name = event['substitution_replacement']
+                lineup = current_lineups.get(team_id).get("players")
+                outgoing_player = None
+                for i, p in enumerate(lineup):
+                    if p["player_name"] == sub_player_id:
+                        outgoing_player = p
+                        lineup[i] = {
+                            'player_id': incoming_player_id, 
+                            'player_name': incoming_player_name, 
+                            'position_id': outgoing_player["position_id"],
+                            'position_name': outgoing_player["position_name"],
+                            'jersey_number': None
+                        }
+                        break
+
+                team_states.append({
+                    'team_id': team_id, 
+                    'team_name': event["team_name"],
+                    'minute': event['minute'], 
+                    'second': event['second'],
+                    'event_type': "Substitution",
+                    "formation": current_lineups[team_id]["formation"],
+                    "lineup": current_lineups[team_id]["players"],
+                    "substituted_in": incoming_player_name,
+                    "substituted_out": sub_player_id
+                })  
+    
+            elif event["type.name"] == "Tactical Shift":
+                team_id = event["team"]
+                current_lineups[team_id] = {
+                    'formation': event["tactics"].get('formation'),
+                    "players": [{
+                        "player_id": p["player"]["id"],
+                        "player_name": p["player"]["name"],
+                        "position_id": p["position"]["id"],
+                        "position_name": p["position"]["name"],
+                        "jersey_number": p["jersey_number"]
+                    } for p in event["tactics"].get("lineup")
+                    ]
+                }   
+
+                team_states.append({
+                'team_id': team_id,
+                'team_name': event['team_name'],
+                'minute': event['minute'],
+                'second': event['second'],
+                'event_type': 'Tactical Shift',
+                'formation': event['tactics'].get('formation'),
+                'lineup': current_lineups[team_id]['players'].copy(),
+                'substituted_in': None,  
+                'substituted_out': None
+            })
+
     df_lineups = pd.DataFrame(team_states)
-    df_players = df_lineups.explode("lineup").reset_index(drop=True)
-    player_data = df_players["lineup"].apply(pd.Series)
-    df_tactics_final = pd.concat([df_players.drop(columns=["lineup"]), player_data], axis=1)
-    df_main_final = df[~df["type.name"].isin(["Starting XI", "Tactical Shift", "Substitution"])]
+    if not df_lineups.empty:
+        df_lineups = df_lineups.explode('lineup').reset_index(drop=True)
+        player_data = df_lineups['lineup'].apply(pd.Series)
+        df_tactics_final = pd.concat([df_lineups.drop(columns=['lineup']), player_data], axis=1)
+
+    else:
+        df_tactics_final = pd.DataFrame()
+
+    df_main_final = df[~df["type.name"].isin(["Starting XI", "Substitution", "Tactical Shift"])].copy()
+
     return df_tactics_final, df_main_final
